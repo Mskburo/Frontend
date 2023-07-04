3d764da78cf2b73c8d7273259c5a6e7eec90a142
Arsipa <senya.suvorov2@gmail.com>
2023-07-04 14:36:48 +0300

    test site

--- src/App.vue
+++ src/App.vue
@@ -1,26 +1,208 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <div class="container">
+        <section class="admin row">
+            <button @click="this.showCategories = !this.showCategories">
+                скрыть\открыть Категории
+            </button>
+            <button @click="this.showTickets = !this.showTickets">
+                скрыть\открыть билеты
+            </button>
+        </section>
+        <section v-if="this.showCategories">
+            <div class="categories row">
+                <button
+                    @click="changeCategory(0)"
+                    :class="{ active: this.currentCategory === 0 }">
+                    Все
+                </button>
+                <button
+                    @click="changeCategory(1)"
+                    :class="{ active: this.currentCategory === 1 }">
+                    Категория 1
+                </button>
+                <button
+                    @click="changeCategory(2)"
+                    :class="{ active: this.currentCategory === 2 }">
+                    Категория 2
+                </button>
+            </div>
+            <div class="list column row">
+                <p class="bold row">Отображено: {{ this.listToShow.length }}</p>
+                <div
+                    class="list__item row column"
+                    v-for="item in this.listToShow">
+                    <p>id: {{ item.id }}</p>
+                    <p class="bold">userId: {{ item.userId }}</p>
+                    <p>Название: {{ item.title }}</p>
+                </div>
+            </div>
+        </section>
+        <form
+            v-if="this.showTickets"
+            @submit.prevent="this.createOrder"
+            class="row column tickets">
+            <input
+                type="number"
+                v-model="this.cart.id"
+                placeholder="id экскурсии"
+                required />
+            <p class="list__item row">
+                <input
+                    @input="this.calcTotal"
+                    type="number"
+                    v-model="this.cart.child"
+                    placeholder="количество детей" />
+                <span>Цена: 100</span>
+            </p>
+            <p class="list__item row">
+                <input
+                    @input="this.calcTotal"
+                    type="number"
+                    v-model="this.cart.parents"
+                    placeholder="количество взрослых" />
+                <span>Цена: 300</span>
+            </p>
+            <p class="list__item row">
+                <input
+                    @input="this.calcTotal"
+                    type="number"
+                    v-model="this.cart.old"
+                    placeholder="количество пенсионеров" />
+                <span>Цена: 200</span>
+            </p>
+            <p class="bold row">Сумма: {{ this.cart.totalPrice }}</p>
+            <button type="submit">Оформить</button>
+        </form>
+    </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import axios from "axios";
 
 export default {
-  name: 'App',
-  components: {
-    HelloWorld
-  }
-}
+    name: "MskBuro",
+    components: {},
+    data() {
+        return {
+            list: [{ id: -1, title: "Не загрузилось)" }],
+            listToShow: [],
+            currentCategory: 0,
+            showCategories: true,
+            showTickets: true,
+            cart: {
+                totalPrice: 0,
+                id: null,
+                child: null,
+                parents: null,
+                old: null,
+            },
+        };
+    },
+    methods: {
+        calcTotal() {
+            this.cart.totalPrice =
+                this.cart.child * 100 +
+                this.cart.parents * 300 +
+                this.cart.old * 200;
+        },
+        createOrder() {
+            if (this.cart.totalPrice === 0) {
+                alert("Вы ничего не выбрали");
+            }
+            axios
+                .post("http://localhost:80/createOrder", {
+                    id: this.cart.id,
+                    child: this.cart.child,
+                    parents: this.cart.parents,
+                    old: this.cart.old,
+                })
+                .then((response) => {
+                    console.log(response);
+                })
+                .catch((error) => {
+                    console.log(error);
+                });
+        },
+        changeCategory(categoryId) {
+            if (categoryId === 0) {
+                this.listToShow = this.list;
+            } else {
+                this.listToShow = this.list.filter(
+                    (a) => a.userId === categoryId
+                );
+            }
+            this.currentCategory = categoryId;
+        },
+    },
+    created() {
+        axios
+            .get("https://jsonplaceholder.typicode.com/posts")
+            .then((response) => {
+                this.list = response.data;
+                this.listToShow = this.list;
+            });
+    },
+};
 </script>
 
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+* {
+    margin: 0;
+    padding: 0;
+    box-sizing: border-box;
+    font-family: inherit;
+}
+
+.container {
+    width: 1200px;
+    padding: 0 20px;
+    margin: 0 auto;
+    box-sizing: content-box;
+}
+
+body {
+    font-family: Roboto, sans-serif;
+}
+
+.row {
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    gap: 20px;
+}
+.column {
+    flex-direction: column;
+}
+.list__item {
+    padding: 10px;
+    border: 2px solid black;
+}
+.categories {
+    margin-top: 50px;
+    margin-bottom: 30px;
+}
+button {
+    padding: 10px;
+    border: 2px solid black;
+    border-radius: 10px;
+    font-size: 20px;
+    background: none;
+    color: black;
+    transition: 0.3s;
+    cursor: pointer;
+}
+button.active,
+button:hover {
+    background: black;
+    color: white;
+}
+.bold {
+    font-weight: bold;
+}
+.tickets {
+    padding: 50px 0;
+}
+.tickets > * {
+    font-size: 20px;
 }
 </style>
