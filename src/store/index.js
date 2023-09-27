5329c8324b8e84be8fb30afda4227d45e93b63ce
Arsipa <senya.suvorov2@gmail.com>
2023-09-27 16:35:03 +0300

    finish media, add .env api_url

--- src/store/index.js
+++ src/store/index.js
@@ -2,7 +2,7 @@ import { createStore } from "vuex";
 
 export default createStore({
     state: {
-        API_URL: "http://localhost:8090/api/v1",
+        API_URL: process.env.VUE_APP_API_URL,
         popupName: "",
         popupInfo: "",
     },
