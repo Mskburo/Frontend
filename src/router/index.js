c05d3e986b0c0c882d60dda6e32f7e9d482cb998
fanadape <fanadape@gmail.com>
2024-06-08 21:12:15 +0300

    feat: add new view - promo

--- src/router/index.js
+++ src/router/index.js
@@ -2,21 +2,26 @@ import { createRouter, createWebHistory } from 'vue-router'
 import Index from '../views/Index.vue'
 
 const routes = [
-  {
-    path: '/',
-    name: 'Index',
-    component: Index
-  },
-  {
-    path: '/payment-success',
-    name: 'PaymentSuccess',
-    component: () => import('../views/PaymentSuccess.vue')
-  },
+	{
+		path: '/',
+		name: 'Index',
+		component: Index,
+	},
+	{
+		path: '/payment-success',
+		name: 'PaymentSuccess',
+		component: () => import('../views/PaymentSuccess.vue'),
+	},
+	{
+		path: '/promo/:id',
+		name: 'Promo',
+		component: () => import('../views/Promo.vue'),
+	},
 ]
 
 const router = createRouter({
-  history: createWebHistory(),
-  routes
+	history: createWebHistory(),
+	routes,
 })
 
 export default router
---
+++ src/views/Promo.vue
@@ -0,0 +1,26 @@
+<template>
+	<div></div>
+</template>
+
+<script>
+import { onMounted, ref } from 'vue'
+import { useRoute, useRouter } from 'vue-router'
+
+export default {
+	setup() {
+		const router = useRouter()
+		const route = useRoute()
+		const promoId = ref(null)
+
+		onMounted(() => {
+			promoId.value = route.params.id
+			localStorage.setItem('promo_id', promoId.value)
+			router.push('/')
+		})
+
+		return {}
+	},
+}
+</script>
+
+<style lang="scss" scoped></style>
